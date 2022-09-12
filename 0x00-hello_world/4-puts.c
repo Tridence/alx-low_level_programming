3a6af7ab3e6220fc8ad3ef5c3bb8fe5ebf1c845b
tridence <brianwanjalabarasa@gmail.com>
2022-09-12 22:50:04 +0300

    Fixed Betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-/*
- * main - prints out the string
- *
- * Description: using the puts function
- * prints 'program prints programming languages being multilingual'
- * return: 0
- */
+/**
+ *main- Serves as the entry point for the program
+*Return: By default returns zero if no erros otherwise returns non-zero value.
+*/
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
