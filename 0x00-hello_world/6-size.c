54c756ff4ecb9770c6d99852901919467df7435e
tridence <brianwanjalabarasa@gmail.com>
2022-09-12 22:55:14 +0300

    Added a New Line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-/*
- * main - print the string in the print function
- *
- * Description: Printing the size o various comp types
- * this program prints the prints on comp sizes
- * return: 0
- */
-int main(void) 
-{
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
 
-	printf("Print size of char: %d byte(s)\n", sizeof(c));
-	printf("print size of int: %d byte(s)\n", sizeof(i));
-	printf("Print size of long: %d byte(s)\n", sizeof(li));
-	printf("Print size of long long: %d byte(s)\n", sizeof(lli));
-	printf("Print size of float: %d byte(s)\n", sizeof(f));
-	return (0);
+/**
+ * main-entry point for the program.
+ * Return: zero if no errors, return non zero if errors
+ */
+
+int main(void)
+
+{
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
+return (0);
+
 }
