9b22587536538bf4321a7e343d3be0fca754f945
tridence <brianwanjalabarasa@gmail.com>
2022-09-12 22:45:25 +0300

    Changed Permissions

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - prints out the string
+ *
+ * Description: using the puts function
+ * prints 'program prints programming languages being multilingual'
+ * return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);	
+}
