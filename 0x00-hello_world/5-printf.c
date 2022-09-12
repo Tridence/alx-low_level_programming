9f9312607063717c1e87a05882e8ae3d14bd9125
tridence <brianwanjalabarasa@gmail.com>
2022-09-12 22:51:43 +0300

    Fixed Betty coding style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-/*
- * main - prints the string
- *
- * Description: using the main function
- * Return: 0
+/**
+ * main-entry point for the program.
+*
+ * Return:0 if no errors, return non zero if errors
  */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
